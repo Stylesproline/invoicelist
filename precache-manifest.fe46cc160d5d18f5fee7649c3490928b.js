8c853ae3ecd1175488e22430e5cd8dd578b21f71
Stylesproline <stylesproline@gmail.com>
2021-11-25 19:52:34 +0200

    Updates

---
+++ precache-manifest.fe46cc160d5d18f5fee7649c3490928b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba38c59cb849b37eae3138bbd0176633",
+    "url": "/invoicelist/index.html"
+  },
+  {
+    "revision": "1b67d289d397cbc8c89d",
+    "url": "/invoicelist/static/css/2.437716dd.chunk.css"
+  },
+  {
+    "revision": "982452df0e2f83f992d8",
+    "url": "/invoicelist/static/css/main.ee18f44e.chunk.css"
+  },
+  {
+    "revision": "1b67d289d397cbc8c89d",
+    "url": "/invoicelist/static/js/2.dc4098bf.chunk.js"
+  },
+  {
+    "revision": "c5f92507b76b3fbc3f8e5f5fe0c84cf3",
+    "url": "/invoicelist/static/js/2.dc4098bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "982452df0e2f83f992d8",
+    "url": "/invoicelist/static/js/main.a179765e.chunk.js"
+  },
+  {
+    "revision": "032543ef7abbc5ed1a48",
+    "url": "/invoicelist/static/js/runtime-main.aeace734.js"
+  },
+  {
+    "revision": "5af81432dda1dde2def604f8052f424a",
+    "url": "/invoicelist/static/media/download.5af81432.svg"
+  },
+  {
+    "revision": "15c01d9b81ceb6702aa92e3b5416ce50",
+    "url": "/invoicelist/static/media/remove.15c01d9b.svg"
+  },
+  {
+    "revision": "d4da359f65106dae13fb51495f035419",
+    "url": "/invoicelist/static/media/resize.d4da359f.svg"
+  }
+]);
